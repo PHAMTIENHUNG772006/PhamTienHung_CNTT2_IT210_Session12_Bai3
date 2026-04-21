b49d7bfe5c7e0aad71f07bd2d925f840a48c8ff6
Phạm Tiến Hưng <hungpham772006@gmail.com>
2026-04-21 18:26:35 +0700

    commit code

--- src/main/java/com/re/session12_bai1/HositalComponent.java
+++ src/main/java/com/re/session12_bai1/HositalComponent.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HositalController {
+public class HositalComponent {
 
     @Value("${hospital.name}")
     private String hospitalName;
